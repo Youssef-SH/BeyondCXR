117843ee2208d6c34de8e4cee5779dc4a36b1cc8
Youssef-SH <youssef.shili.contact@gmail.com>
2026-08-08 22:17:43 +0100

    feat(symile): add supervised laboratory preprocessing

--- src/radfusion/utils/skops_io.py
+++ src/radfusion/utils/skops_io.py
@@ -15,6 +15,7 @@ TRUSTED_MODEL_TYPES = frozenset(
         "lightgbm.basic.Booster",
         "lightgbm.sklearn.LGBMClassifier",
         "numpy.dtype",
+        "radfusion.data.symile_preprocess.SymileLabEcdfTransformer",
         "radfusion.data.tabular_preprocess.RsnaMetadataFeatures",
     }
 )
